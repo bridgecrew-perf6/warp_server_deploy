54421532b30419a168a0cd4e57af172b3f4f5a94
Darren Picard <darren.picard@neofinancial.com>
2022-03-01 22:23:56 -0700

    Setup githib ci to push build image to digital ocean registry

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM rust as builder
+FROM rust:latest as builder
 
 WORKDIR /app
 COPY . .
 
 RUN cargo build --release
 
-FROM rust:latest-alpine
+FROM debian:buster-slim
 
-COPY --from=builder /target/release/warp_server_deploy /warp_server_deploy
+COPY --from=builder /app/target/release/warp_server_deploy /warp_server_deploy
 
 CMD ["/warp_server_deploy"]
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello_world = warp::get().map(|| String::from("Hello World!"));
+
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     let sum = warp::path!("sum" / u32 / u32).map(|a, b| format!("{} + {} = {}", a, b, a + b));
-    warp::serve(hello.or(sum)).run(([127, 0, 0, 1], 3030)).await;
+
+    println!("Starting Server on port 8000");
+
+    warp::serve(hello.or(sum).or(hello_world))
+        .run(([0, 0, 0, 0], 8000))
+        .await;
 }
